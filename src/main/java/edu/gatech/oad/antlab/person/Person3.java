dffd0f2b51954c98dfdc0eae729098d3d3be4892
tjones320 <tjones320@gatech.edu>
2017-09-07 17:34:18 -0400

    fixed temp bug

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,7 +43,7 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  temp = "";
+	  String temp = "";
 	  for (int i = input.length - 1; i >= 0; i++) {
 	  	temp+= input.getChar(i);
 	  }
