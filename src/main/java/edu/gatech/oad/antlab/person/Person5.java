03004d25bcc1c0bb1a99bc05d61a46ef439611a4
Emilee Sisson <Emilee@lawn-128-61-42-24.lawn.gatech.edu>
2017-09-06 21:22:59 -0400

    Updated Person5.java file

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Emilee Sisson
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,13 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  String scramble = "";
+	  for (int i = 2; i < input.length(); i++) {
+	  	scramble += input.charAt(i);
+	  }
+	  scramble += input.charAt(0);
+	  scramble += input.charAt(1);
+	  return scramble;
 	}
 	
 	/**
